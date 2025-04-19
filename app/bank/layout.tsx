2fdb6d469aa97c018e67c73041592d4ab931bbc5
krnkiran22 <kirandev2210@gmail.com>
2025-04-19 12:49:00 +0530

    integration of onchainkit

---
+++ app/bank/layout.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from "react"
+
+import "@coinbase/onchainkit/styles.css" // Add OnchainKit styles
+import { Inter } from "next/font/google"
+
+
+
+import { Providers } from "@/app/bank/providers"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+       
+          <Providers>
+           
+              {children}
+             
+           
+          </Providers>
+     
+      </body>
+    </html>
+  )
+}
+
+
+
+
+
+export const metadata = {
+      generator: 'v0.dev'
+    };
